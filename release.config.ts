d5960c4a3757900c487ca9924fb9f38ef3912ddd
mlypn <nick.lipnyagov@gmail.com>
2024-07-13 17:42:31 +0300

    Update semantic-release configuration

---
+++ release.config.ts
@@ -0,0 +1,19 @@
+import { Options } from 'semantic-release';
+
+export const config: Options = {
+    branches: ['master'],
+    plugins: [
+        '@semantic-release/commit-analyzer',
+        '@semantic-release/release-notes-generator',
+        '@semantic-release/changelog',
+        '@semantic-release/npm',
+        '@semantic-release/github',
+        [
+            '@semantic-release/git',
+            {
+                assets: ['package.json', 'CHANGELOG.md'],
+                message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+            },
+        ],
+    ],
+};
