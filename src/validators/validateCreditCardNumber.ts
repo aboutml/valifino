14a8cbe044b30504ae67989b6f62d07f61aaaa13
mlypn <nick.lipnyagov@gmail.com>
2024-07-15 16:49:50 +0300

    feat: add credit card number validation

---
+++ src/validators/validateCreditCardNumber.ts
@@ -0,0 +1,20 @@
+export const isValidCreditCardNumber = (creditCardNumber: string): boolean => {
+  const digits = creditCardNumber.replace(/\D/g, '').split('').map(Number);
+
+  let sum = 0;
+  let isSecond = false;
+
+  for (let i = digits.length - 1; i >= 0; i--) {
+    let digit = digits[i];
+    if (isSecond) {
+      digit *= 2;
+      if (digit > 9) {
+        digit -= 9;
+      }
+    }
+    sum += digit;
+    isSecond = !isSecond;
+  }
+
+  return sum % 10 === 0;
+};
---
+++ test/validators/validateCreditCardNumber.spec.ts
@@ -0,0 +1,37 @@
+import { isValidCreditCardNumber } from '../../src';
+
+describe('validateCreditCardNumber', () => {
+  describe('#isValidCreditCardNumber', () => {
+    [
+      {
+        cardNumber: '4111111111111111',
+        description: 'should return true for a valid Visa card number',
+        expectedValue: true,
+      },
+      {
+        cardNumber: '5500000000000004',
+        description: 'should return true for a valid MasterCard number',
+        expectedValue: true,
+      },
+      {
+        cardNumber: '1234567890123456',
+        description: 'should return false for an invalid card number',
+        expectedValue: false,
+      },
+      {
+        cardNumber: '411111111111111',
+        description: 'should return false for a card number with invalid length',
+        expectedValue: false,
+      },
+      {
+        cardNumber: '4111111111111abc',
+        description: 'should return false for a card number with letters',
+        expectedValue: false,
+      },
+    ].forEach(({ cardNumber, description, expectedValue }) => {
+      it(description, () => {
+        expect(isValidCreditCardNumber(cardNumber)).toBe(expectedValue);
+      });
+    });
+  });
+});
