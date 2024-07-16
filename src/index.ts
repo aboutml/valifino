02808b51d803bf0e74f5644837675be8bb2fbd1f
Mykyta Lypniahov <80581736+aboutml@users.noreply.github.com>
2024-07-16 18:21:02 +0300

    fix: update compiler options (#20)

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+// Validators
 export { isValidCreditCardNumber } from './validators/validateCreditCardNumber';
 export { isValidExpirationDate } from './validators/validateExpirationDate';
 export { isValidCVV } from './validators/validateCVV';
+
+// Types
+export { CardType } from './types/CardType';
